b5aa5148243ff44ca584fa51a5cf1d3dad41bb66
Ryan Skeete <skeeteryan@gmail.com>
2020-12-23 09:32:18 -0500

    Update and rename data.cmd to data_dictionary.cmd
    Data description

---
+++ Data/data_dictionary.cmd
@@ -0,0 +1,15 @@
+•	Utilized the OECD PISA Data Explorer to identify variables of interest for 43 countries including all OECD members and 6 non-members over 7 triennial years (2000-2018)
+•	Target Variables (overall and by gender):
+–	Reading scores (ReadingScoreAvg)
+–	Mathematics  scores (MathScoresAvg)
+–	Science scores (ScienceScoreAvg)
+–	All scores above are average normalized scores by country with mean 500 and ranges from 0 to 1000.
+•	Predictor Variables:
+–	The proportion of students' heritage language different from testing language (PerDiffHeritageLang)
+–	An index of Highest occupation status of parents ranging from 0 to 90 (HighestOccupParentsStatiusIndex)
+–	Proportion of students living with 2 parents (Per2parents)
+–	The index of economic social and cultural status ranging from –3 to 3 (EconSocCulIndex)
+•	Additional Predictor variables (from World Bank’s Development Indicators):
+–	GDP per capita measured at current PPP dollars (GDPperCapPPP)
+–	Government education expenditure as a percentage of GDP (GovEducExpPercentGDP)
+
