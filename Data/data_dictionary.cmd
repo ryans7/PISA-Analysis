171e8d6fd7fd4a22adc29cebe4a71cf59477408f
Ryan Skeete <skeeteryan@gmail.com>
2020-12-28 08:36:50 -0500

    Update data_dictionary.cmd

--- Data/data_dictionary.cmd
+++ Data/data_dictionary.cmd
@@ -1,15 +1,27 @@
-•	Utilized the OECD PISA Data Explorer to identify variables of interest for 43 countries including all OECD members and 6 non-members over 7 triennial years (2000-2018)
+•	•	Utilized the OECD PISA Data Explorer to identify variables of interest for 43 countries including all OECD members and 6 non-members over 7 triennial years (2000-2018)
 •	Target Variables (overall and by gender):
-–	Reading scores (ReadingScoreAvg)
-–	Mathematics  scores (MathScoresAvg)
-–	Science scores (ScienceScoreAvg)
+–	Reading scores (reading_score_avg)
+–	Female reading scores (reading_score_female_avg)
+–	Male reading scores (reading_score_male_avg)
+–	Mathematics  scores (math_score_avg)
+–	Female math scores (math_score_female_avg)
+–	Male math scores (math_score_male_avg)
+–	Science scores (science_score_avg)
+–	Female science scores (science_score_female_avg)
+–	Male science scores (science_score_male_avg)
 –	All scores above are average normalized scores by country with mean 500 and ranges from 0 to 1000.
 •	Predictor Variables:
-–	The proportion of students' heritage language different from testing language (PerDiffHeritageLang)
-–	An index of Highest occupation status of parents ranging from 0 to 90 (HighestOccupParentsStatiusIndex)
-–	Proportion of students living with 2 parents (Per2parents)
-–	The index of economic social and cultural status ranging from –3 to 3 (EconSocCulIndex)
+–	The proportion of students' heritage language different from testing language (per_diff_heritage_lang)
+–	An index of Highest occupation status of parents ranging from 0 to 90 (highest_occup_parents_status_index)
+–	Proportion of students living with 2 parents (per_2_parents)
+–	The index of economic social and cultural status ranging from –3 to 3 (econ_soc_cul_index)
+–	Percent of country's GDP allocated to education expenditure as a percent of GDP (gov_educ_exp_percent_gdp)
+–	Percent of country's GDP allocated to secondary education expenditure as a percent of GDP (gov_exp_secondary_educ_per_student)
+–	Country (jurisdiction)
+–	Region country resides in according to World Bank designation (region)
+–	World Bank Income level classification (income_group)
 •	Additional Predictor variables (from World Bank’s Development Indicators):
-–	GDP per capita measured at current PPP dollars (GDPperCapPPP)
-–	Government education expenditure as a percentage of GDP (GovEducExpPercentGDP)
+–	GDP per capita measured at current PPP dollars (gdp_per_cap_ppp)
+–	Government education expenditure as a percentage of GDP (gov_educ_exp_percent_gdp)
+
 
